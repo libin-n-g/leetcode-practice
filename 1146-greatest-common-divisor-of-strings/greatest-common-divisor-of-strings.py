5b4cd05f52f00f8a0995a84f76e2b724d5822eed
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-05-23 17:40:21 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (25.28%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,16 +1,24 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        n1, n2 = len(str1), len(str2)
-        def isDivisor(s):
-            l = len(s)
-            if n1 % l != 0 and n2 % l != 0:
-                return False
-            if s * (n1//l) == str1 and s * (n2//l) == str2:
-                return True
-            return False
-        for i in range(min(n1, n2), 0, -1):
-            ret = str1[:i]
-            if isDivisor(ret):
-                return ret
-        return ""
+        if str1 + str2 != str2 + str1:
+            return ""
+        a, b = len(str1), len(str2)
+        while b:
+            b, a = a % b, b
+        return str1[:a]
+    # BRUTE FORCE
+    # def gcdOfStrings(self, str1: str, str2: str) -> str:
+    #     n1, n2 = len(str1), len(str2)
+    #     def isDivisor(s):
+    #         l = len(s)
+    #         if n1 % l != 0 and n2 % l != 0:
+    #             return False
+    #         if s * (n1//l) == str1 and s * (n2//l) == str2:
+    #             return True
+    #         return False
+    #     for i in range(min(n1, n2), 0, -1):
+    #         ret = str1[:i]
+    #         if isDivisor(ret):
+    #             return ret
+    #     return ""
         

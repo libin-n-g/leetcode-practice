cca2c0fc5ae583aab20ebf98d42084654f7f766f
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-05-30 16:49:45 -0700

    Time: 30 ms (36.16%) | Memory: 24.9 MB (37.34%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,12 +5,12 @@ class Solution:
         ret[n-1] = nums[n-1]
         for i in range(n-2, -1, -1):
             ret[i] = ret[i+1] * nums[i] 
-        for i in range(1, n):
-            nums[i] *= nums[i-1]
         for i in range(n):
             if 0 < i < n - 1:
+                nums[i] *= nums[i-1]
                 ret[i] = ret[i+1] * nums[i-1]
             elif i == n-1:
+                nums[i] *= nums[i-1]
                 ret[i] = nums[i-1]
             else:
                 ret[i] = ret[i+1]

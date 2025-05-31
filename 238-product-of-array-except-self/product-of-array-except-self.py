23f69120ad36d5c0866507c2ccfcfe0d8687ff0c
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-05-30 17:01:38 -0700

    Time: 20 ms (74.57%) | Memory: 23.2 MB (93.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,10 +3,16 @@ class Solution:
         n = len(nums)
         p = 1
         ret = []
+        # Ret is used to store prefix sum except the last element. 
+        # note that 1 is added in the begining
+        # ret[i] represents product of every element before it (exclusing ith element)
         for num in nums:
             ret.append(p)
             p *= num
         p = 1
+        # Suffix sum. Starts with p == 1 and p value is used from previous iteration. 
+        # This p value will be product of all elements after i index element. 
+        # The p calcullated in i index of loop is product of all elements after index i (including i)
         for i in range(n-1, -1, -1):
             ret[i] = ret[i] * p
             p *= nums[i]

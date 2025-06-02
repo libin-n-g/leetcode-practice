3c12ff7bc2258c618c7c1a0f1ffacd7928a43216
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-01 18:35:59 -0700

    Time: 3 ms (39.03%) | Memory: 17.8 MB (74.91%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,19 +1,35 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        count = 1
-        char = chars[0]
+        final_length = 0
         i = 0
         while i < len(chars):
-            char = chars[i]
-            count = 1
-            i += 1
-            count_index = i
-            while i < len(chars) and char == chars[i]:
-                count+=1
-                chars.pop(i)
-            if count == 1:
-                continue
-            for j, s in enumerate(str(count)):
-                chars.insert(count_index + j,  s)
-                i += 1
-        return len(chars)
+            c = chars[i]
+            group_count = 0
+            while i + group_count < len(chars) and c == chars[i + group_count]:
+                group_count += 1
+            chars[final_length] = c
+            final_length+=1
+            if group_count > 1:
+                for digit in str(group_count):
+                    chars[final_length] = digit
+                    final_length+=1
+            i += group_count
+        return final_length
+            
+        # count = 1
+        # char = chars[0]
+        # i = 0
+        # while i < len(chars):
+        #     char = chars[i]
+        #     count = 1
+        #     i += 1
+        #     count_index = i
+        #     while i < len(chars) and char == chars[i]:
+        #         count+=1
+        #         chars.pop(i)
+        #     if count == 1:
+        #         continue
+        #     for j, s in enumerate(str(count)):
+        #         chars.insert(count_index + j,  s)
+        #         i += 1
+        # return len(chars)

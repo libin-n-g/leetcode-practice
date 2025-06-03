abf9896abacd9f671809d15eef4b47e418ed2885
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-03 14:22:01 -0700

    Time: 3 ms (49.34%) | Memory: 69.5 MB (63.46%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
@@ -1,20 +1,19 @@
 
 function findMaxAverage(nums: number[], k: number): number {
     let sum: number = 0;
-    let max_average : number = 0;
+    let max_sum : number = 0;
     for ( let i = 0; i < nums.length; i++) {
         let temp = 0;
         if (i < k) {
             sum += nums[i];
-            max_average = sum / k;
+            max_sum = sum;
         } else {
             sum -= nums[i-k];
             sum += nums[i];
-            temp = sum / k;
-            if (temp > max_average) {
-                max_average = temp;
+            if (sum > max_sum) {
+                max_sum = sum;
             }
         }
     }
-    return max_average;
+    return max_sum / k;
 };

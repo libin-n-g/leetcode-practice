6e3c94d0968de3077412a5b8060692bec2e00aa9
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-03 13:42:20 -0700

    Time: 7 ms (0.00%) | Memory: 9.4 MB (62.50%) - LeetSync

---
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.go
@@ -0,0 +1,27 @@
+
+func maxCandies(status []int, candies []int, keys [][]int, containedBoxes [][]int, initialBoxes []int) int {
+    var not_opened_but_visited map[int]bool = map[int]bool{}
+    var ret int = 0
+    for len(initialBoxes) != 0 {
+        curr_box := initialBoxes[0]
+        initialBoxes = initialBoxes[1:]
+        if status[curr_box] == 1{
+            ret += candies[curr_box]
+            for _, key := range keys[curr_box] {
+                status[key] = 1
+                if _, ok := not_opened_but_visited[key]; ok {
+                    initialBoxes = append(initialBoxes, key)
+                    delete(not_opened_but_visited, key)
+                }
+            }
+            for _, box := range containedBoxes[curr_box] {
+                fmt.Println(box)
+                initialBoxes = append(initialBoxes, box)
+            }
+        } else {
+            not_opened_but_visited[curr_box] = true
+        }
+    }
+
+    return ret
+}

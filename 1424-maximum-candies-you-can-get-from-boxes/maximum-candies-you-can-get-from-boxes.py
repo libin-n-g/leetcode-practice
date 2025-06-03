49b7831376d6f94bac89fd84d737a4a294ddc5f0
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-03 11:06:54 -0700

    Time: 19 ms (65.90%) | Memory: 28 MB (96.93%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
@@ -2,8 +2,6 @@ class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
         # Initialize a queue with the initial boxes that can be opened
         queue = deque(initialBoxes)
-        # Track visited boxes to avoid processing the same box multiple times
-        visited = set()
         # Track visited boxes that can't be opened yet due to missing keys
         unable_to_open_visited = set()
         # Initialize the total candies collected
@@ -21,7 +19,8 @@ class Solution:
                 
                 # Process all boxes contained within the current box
                 for i in containedBoxes[curr_node]:
-                    queue.append(i)
+                    # No cycles possible as tree structure is guaranteed. 
+                    queue.append(i) 
                 
                 # Process all keys found in the current box
                 for i in keys[curr_node]:
@@ -32,9 +31,6 @@ class Solution:
                         queue.append(i)
                         # remove item from the set as this is no longer unopenable
                         unable_to_open_visited.remove(i)
-                
-                # Mark the current box as visited
-                visited.add(curr_node)
             else:
                 # If the box can't be opened (locked), add it to not_opened set
                 unable_to_open_visited.add(curr_node)

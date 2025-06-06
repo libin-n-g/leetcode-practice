1bff8ba1e0a78af3b0aee4001a6be9dd56f5b29b
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-06 13:55:34 -0700

    Time: 716 ms (23.45%) | Memory: 20.8 MB (59.66%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,5 +1,18 @@
 from string import ascii_lowercase
 class Solution:
+    # def robotWithString(self, s: str) -> str:
+        # i = 0
+        # t = []
+        # paper = ""
+        # for c in ascii_lowercase:
+        #     if c not in c:
+        #         continue
+        #     else:
+        #         while t and t[-1] <= c:
+        #             paper += t.pop()
+        #         while c in s:
+        #             while s[-1] != c:
+
     def robotWithString(self, s: str) -> str:
         counter = Counter(s)
         p = ""
@@ -13,7 +26,6 @@ class Solution:
                     if counter[smallest_freq_char] > 0:
                         break
                     smallest_freq_char = chr(ord(smallest_freq_char) + 1)
-                # print(smallest_freq_char)
                 if t[-1] > smallest_freq_char:
                     break
                 else:

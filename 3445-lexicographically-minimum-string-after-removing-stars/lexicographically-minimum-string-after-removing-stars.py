f6968c8a6bcc8a5dbe2f9eaa985d7c6041d0356d
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-07 15:12:23 -0700

    Time: 506 ms (76.62%) | Memory: 22.6 MB (94.81%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,14 +1,18 @@
 class Solution:
     def clearStars(self, s: str) -> str:
-        heap = []
+        stacks = [[] for _ in range(26)]
         keep = [True] * len(s)
         for i, c in enumerate(s):
             if c == "*":
-                _, index = heappop(heap)
-                keep[-index] = False
                 keep[i] = False
+                j = 0
+                while j < 26:
+                    if stacks[j]:
+                        keep[stacks[j].pop()] = False
+                        break
+                    j += 1
                 continue
-            heappush(heap, (c, -i))
+            stacks[ord(c) - ord('a')].append(i)
         ret = ""
         for i, c in enumerate(s):
             if keep[i]:

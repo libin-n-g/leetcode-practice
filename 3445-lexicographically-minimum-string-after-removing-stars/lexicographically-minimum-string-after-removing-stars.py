9b7b47813bda0df1e1862689e75b6f2c30d7e885
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-07 07:53:54 -0700

    Time: 703 ms (35.97%) | Memory: 34.2 MB (5.04%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -6,11 +6,12 @@ class Solution:
             if c == "*":
                 _, index = heappop(heap)
                 min_values.add(-index)
+                min_values.add(i)
                 continue
             heappush(heap, (c, -i))
         ret = ""
         for i, c in enumerate(s):
-            if i not in min_values and c != "*":
+            if i not in min_values:
                 ret += c
         return ret
 

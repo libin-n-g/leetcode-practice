d541ebc2b29506e1a547f70608f0860e29fdbd24
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-07 07:56:25 -0700

    Time: 635 ms (41.01%) | Memory: 29.1 MB (33.09%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,17 +1,17 @@
 class Solution:
     def clearStars(self, s: str) -> str:
         heap = []
-        min_values = set()
+        keep = [True] * len(s)
         for i, c in enumerate(s):
             if c == "*":
                 _, index = heappop(heap)
-                min_values.add(-index)
-                min_values.add(i)
+                keep[-index] = False
+                keep[i] = False
                 continue
             heappush(heap, (c, -i))
         ret = ""
         for i, c in enumerate(s):
-            if i not in min_values:
+            if keep[i]:
                 ret += c
         return ret
 

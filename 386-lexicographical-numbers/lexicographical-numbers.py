014a0ac6eab9b044680797b93ed3a68d1e2c3116
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-08 17:33:36 -0700

    Time: 7 ms (95.39%) | Memory: 21.2 MB (77.90%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,19 +1,31 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         ret = []
-        def traverse_node(prev):
-            ret = []
-            value = prev*10
-            if 0 < value <= n:
-                ret.append(value)
-                ret = ret + traverse_node(value)
-            for num in range(1, 10):
-                value = prev*10 + num
-                # print(value)
-                if value <= n:
-                    ret.append(value)
-                else:
-                    break
-                ret = ret + traverse_node(value)
-            return ret 
-        return traverse_node(0)
+        num = 1
+        while len(ret) < n:
+            ret.append(num)
+            if num *10 <= n:
+                num *= 10
+            else:
+                while num % 10 == 9 or num == n:
+                    num //= 10
+                num += 1
+        return ret
+
+        # ret = []
+        # def traverse_node(prev):
+        #     ret = []
+        #     value = prev*10
+        #     if 0 < value <= n:
+        #         ret.append(value)
+        #         ret = ret + traverse_node(value)
+        #     for num in range(1, 10):
+        #         value = prev*10 + num
+        #         # print(value)
+        #         if value <= n:
+        #             ret.append(value)
+        #         else:
+        #             break
+        #         ret = ret + traverse_node(value)
+        #     return ret 
+        # return traverse_node(0)

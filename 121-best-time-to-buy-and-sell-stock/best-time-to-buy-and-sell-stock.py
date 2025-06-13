e889f03b895f323160434cf2aca33bc74b2bb429
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-12 17:16:40 -0700

    Time: 83 ms (61.20%) | Memory: 26.9 MB (33.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,21 +4,16 @@ class Solution:
         buy_day = 0
         # Initialize profit to store the maximum profit achievable
         profit = 0
-        # Start sell_day from index 1 since we need to sell after buying
-        sell_day = 1
-
         # Iterate through the array while sell_day is within bounds
-        while sell_day < len(prices):
+        for sell_day, sell_price in enumerate(prices):
             # If price at buy_day is less than price at sell_day, we can make a profit
-            if prices[buy_day] < prices[sell_day]:
+            if prices[buy_day] < sell_price:
                 # Update profit if current profit (sell_price - buy_price) is higher
-                profit = max(prices[sell_day] - prices[buy_day], profit)
+                profit = max(sell_price - prices[buy_day], profit)
             else:
                 # If current price is lower than or equal to buy_day price,
                 # update buy_day to current day as it could lead to higher profit later
                 buy_day = sell_day
-            # Move to the next day for selling
-            sell_day += 1
         
         # Return the maximum profit found (0 if no profit is possible)
         return profit

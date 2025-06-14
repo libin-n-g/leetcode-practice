e0a759f5485ca90d7ee1983de0e61252ad23e937
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-14 09:38:40 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.44%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        digits = []
+        while num:
+            digits.append(num % 10)
+            num = num // 10
+        print(digits)
+        min_replacement_digit = None
+        max_replacement_digit = None
+        for d in digits[::-1]:
+            if d != 9 and max_replacement_digit is None:
+                max_replacement_digit = d
+            if d != 0 and min_replacement_digit is None:
+                min_replacement_digit = d
+        diff = 0
+        for p, d in enumerate(digits):
+            if d == max_replacement_digit == min_replacement_digit:
+                current_digit = 9 
+            elif d == max_replacement_digit:
+                current_digit = 9 - d
+            elif d == min_replacement_digit:
+                current_digit = d 
+            else: 
+                current_digit = 0
+            diff += (current_digit*10**p)
+        return diff
+

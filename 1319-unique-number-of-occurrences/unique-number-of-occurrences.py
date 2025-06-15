14ebaa29ffe82576767c9a7e426de970e9c28f16
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-15 13:38:00 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.76%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,9 +1,4 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         counter = Counter(arr)
-        seen_count = set()
-        for key in counter:
-            if counter[key] in seen_count:
-                return False
-            seen_count.add(counter[key])
-        return True
+        return len(set(counter.values())) == len(counter.keys())

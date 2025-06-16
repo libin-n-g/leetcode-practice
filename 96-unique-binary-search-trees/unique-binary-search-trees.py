033a952da4e032082307c85014f2d224da8e818e
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-06-15 17:34:46 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.84%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,14 +1,30 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        dp = [0]*(n+1)
+        # Initialize a DP array of size n+1 with zeros to store the number of unique BSTs for each number of nodes
+        dp = [0] * (n + 1)
+        
+        # Base case: For 0 nodes (empty tree), there is 1 possible tree (the empty tree)
         dp[0] = 1
+        
+        # Base case: For 1 node, there is 1 possible tree (a single node)
         dp[1] = 1
-        def get_num_trees(n):
-            if n in dp:
-                return dp[n]
+        
+        # Iterate from 1 to n-1 to compute the number of unique BSTs for each number of nodes up to n
+        # note that t means t+1 nodes in the tree
+        for t in range(1, n):
+            # Initialize a variable to store the total number of unique BSTs for t+1 nodes
             num = 0
-            for i in range(n):
-                num += get_num_trees(i) * get_num_trees(n-i-1)
-            dp[n] = num
-            return num
-        return get_num_trees(n)
+            
+            # Iterate over all possible root nodes (from 0 to t) to calculate the number of trees
+            for i in range(t + 1):
+                # For each root i, the number of trees is the product of:
+                # - Number of trees possible with i nodes in the left subtree (dp[i])
+                # - Number of trees possible with (t-i) nodes in the right subtree (dp[t-i])
+                # to find totol combinations we need to multiply
+                num += dp[i] * dp[t - i]
+            
+            # Store the computed number of unique BSTs for t+1 nodes in the DP array
+            dp[t + 1] = num
+        
+        # Return the number of unique BSTs possible with n nodes
+        return dp[n]

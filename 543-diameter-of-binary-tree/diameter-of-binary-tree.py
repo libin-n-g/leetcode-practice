d8df878b19d12ad650e6d577a14f68c460bbb9e0
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-07 00:39:52 -0700

    Time: 3 ms (94.38%) | Memory: 20.9 MB (21.49%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,26 +6,36 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        stack = []
-        max_height_map = {}
-        max_diameter = 0
-        if root:
-            stack.append((root, False))
-        while stack:
-            node, visited = stack.pop()
-            if visited:
-                if node.left is None:
-                    left_height = 0
-                else:
-                    left_height = max_height_map[node.left]
-                if node.right is None:
-                    right_height = 0
-                else:
-                    right_height = max_height_map[node.right]
-                max_diameter = max(max_diameter, right_height + left_height)
-                max_height_map[node] = max(left_height, right_height) + 1
-            else:
-                stack.append((node, True))
-                if node.left: stack.append((node.left, False))
-                if node.right: stack.append((node.right, False))
-        return max_diameter
+        self.diameter = 0
+        def depth(root):
+            if not root:
+                return 0
+            left_height, right_height = depth(root.left), depth(root.right)
+            self.diameter = max(self.diameter, left_height + right_height)
+            return max(left_height, right_height) + 1
+        depth(root)
+        return self.diameter
+    # def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    #     stack = []
+    #     max_height_map = {}
+    #     max_diameter = 0
+    #     if root:
+    #         stack.append((root, False))
+    #     while stack:
+    #         node, visited = stack.pop()
+    #         if visited:
+    #             if node.left is None:
+    #                 left_height = 0
+    #             else:
+    #                 left_height = max_height_map[node.left]
+    #             if node.right is None:
+    #                 right_height = 0
+    #             else:
+    #                 right_height = max_height_map[node.right]
+    #             max_diameter = max(max_diameter, right_height + left_height)
+    #             max_height_map[node] = max(left_height, right_height) + 1
+    #         else:
+    #             stack.append((node, True))
+    #             if node.left: stack.append((node.left, False))
+    #             if node.right: stack.append((node.right, False))
+    #     return max_diameter

84a7c9b913fac0e7004c40842a45b6247ff6711b
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-07 21:04:57 -0700

    Time: 229 ms (8.74%) | Memory: 41.9 MB (98.94%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -16,11 +16,14 @@ class Solution:
             while events and current_day == events[0][0]:
                 start, end = heapq.heappop(events)
                 heapq.heappush(sorted_events_by_end_time, end)
-            end_time = heapq.heappop(sorted_events_by_end_time)
-            if end_time >= current_day:
+            
+            while sorted_events_by_end_time and sorted_events_by_end_time[0] < current_day:
+                end_time = heapq.heappop(sorted_events_by_end_time)
+
+            if sorted_events_by_end_time:
+                heapq.heappop(sorted_events_by_end_time)
                 attended += 1
-            if current_day <= end_time:
-                current_day += 1
+            current_day += 1
             if not sorted_events_by_end_time and events:
                 start, end = heapq.heappop(events)
                 heapq.heappush(sorted_events_by_end_time, end)

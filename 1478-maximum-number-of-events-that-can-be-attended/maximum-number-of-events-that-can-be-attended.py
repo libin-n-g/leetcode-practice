093df0a0e9bbb4c4c2eee8a4dcc69783532471ff
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-07 21:09:14 -0700

    Time: 217 ms (10.68%) | Memory: 42 MB (96.91%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -9,21 +9,22 @@ class Solution:
         start, end = heapq.heappop(events)
         heapq.heappush(sorted_events_by_end_time, end)
         while sorted_events_by_end_time:
-            # skip forward to current time
+            # skip forward current time to start of event.
             if current_day < start:
                 current_day = start
-            # add events starting at same starting time. 
+            # add events starting at same current time. 
             while events and current_day == events[0][0]:
-                start, end = heapq.heappop(events)
+                _, end = heapq.heappop(events)
                 heapq.heappush(sorted_events_by_end_time, end)
-            
+            # remove events that have end time less tan today
             while sorted_events_by_end_time and sorted_events_by_end_time[0] < current_day:
                 end_time = heapq.heappop(sorted_events_by_end_time)
-
+            # Add one event to be attented if exists.
             if sorted_events_by_end_time:
                 heapq.heappop(sorted_events_by_end_time)
                 attended += 1
             current_day += 1
+            # No events left to pocess then proced to next starting time of events.
             if not sorted_events_by_end_time and events:
                 start, end = heapq.heappop(events)
                 heapq.heappush(sorted_events_by_end_time, end)

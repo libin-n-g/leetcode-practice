98f855fa67500a759ab407ada3a10f9966883938
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-13 22:45:59 -0700

    Time: 34 ms (93.69%) | Memory: 37 MB (21.98%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
-        total_meeting = 0
+        free_time = startTime[0] # gap of time b/w 0 and 1st event.
         ret = 0
+        startTime.append(eventTime)  # ..... eventTime
+        # # ith event end time is at position i + 1
+        endTime = [0] + endTime  # 0 ......
         N = len(startTime)
-        startTime.append(eventTime)
-        endTime.append(0)
-        for i in range(k):
-            total_meeting += endTime[i] - startTime[i]
-        i = k
-        for i in range(k, N+1):
-            ret = max(ret, startTime[i] - endTime[i - k - 1] - total_meeting)
-            if i < len(endTime):
-                total_meeting += endTime[i] - startTime[i]
-            total_meeting -= (endTime[i - k] - startTime[i - k])
-            
+        for i in range(1, k):
+            # end time of previous event (or starting zero) and start time of ith event.
+            # basically gaps between events
+            free_time += startTime[i] -  endTime[i]
+        for i in range(k, N):
+            # add gap between i th event and i - 1 th event.
+            free_time += startTime[i] - endTime[i]
+            # print(i, i -k, total_meeting)
+            ret = max(ret, free_time)
+            # sutract gap between i - k th event and i -k - 1 th event. 
+            free_time -= (startTime[i - k] - endTime[i - k])
         return ret

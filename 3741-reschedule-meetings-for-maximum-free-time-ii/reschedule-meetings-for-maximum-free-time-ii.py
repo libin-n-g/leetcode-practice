443800d045bf301a663ab4fdbb5947c83cbeb4d4
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-14 14:21:59 -0700

    Time: 228 ms (81.84%) | Memory: 47 MB (5.74%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.py
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.py
@@ -4,17 +4,17 @@ class Solution:
         # Append eventTime to startTime to account for the gap after the last event
         startTime.append(eventTime)
         # Prepend 0 to endTime to represent the start of the timeline
-        endTime = [0] + endTime
+        # endTime = [0] + endTime
         # Get the total number of events (including eventTime)
-        N = len(endTime)
+        N = len(startTime)
         # List to store time gaps between consecutive events
-        time_gaps = []
+        time_gaps = [(startTime[0], 0)]
         # Initialize maximum free time
         max_free_time = 0
         
         # Calculate time gaps between the end of one event and the start of the next
-        for i in range(N):
-            time_gaps.append((startTime[i] - endTime[i], i))
+        for i in range(1, N):
+            time_gaps.append((startTime[i] - endTime[i-1], i))
         
         # Find the three largest time gaps using a heap
         # NOTE: THE TOP 3 time gaps make sure we have next biggest gap even if both gaps are included in current window. 
@@ -23,7 +23,7 @@ class Solution:
         # Iterate through events to calculate maximum free time
         for i in range(1, N):
             # Calculate the duration of the meeting between consecutive events
-            meeting_duration = endTime[i] - startTime[i - 1]
+            meeting_duration = endTime[i-1] - startTime[i - 1]
             # Sum the gaps before and after the current event
             free_time = time_gaps[i][0] + time_gaps[i-1][0]
             # Check if any of the top 3 gaps can replace the meeting duration

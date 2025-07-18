eca1e5241c55fca1630f40e45144a46992376f11
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-18 16:34:59 -0700

    Time: 39 ms (59.03%) | Memory: 18 MB (16.71%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,12 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
         multiplier = 1
+        num = 0
         if x < 0:
             multiplier = -1
             x = -x
         if x >= (1<<31):
             return 0
-        num = 0
         while x >= 1:
             d = x % 10
             num *= 10

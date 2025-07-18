d65901042688a4e2a5dcf38712a593c2701679a0
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-18 16:34:22 -0700

    Time: 31 ms (93.69%) | Memory: 17.9 MB (36.66%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,20 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        digits = []
         multiplier = 1
         if x < 0:
             multiplier = -1
             x = -x
-        print((1<<31))
         if x >= (1<<31):
             return 0
-        while x >= 1:
-            digits.append(x % 10)
-            x //= 10
         num = 0
-        for d in digits:
+        while x >= 1:
+            d = x % 10
             num *= 10
             num += d
+            x //= 10
         if num >= (1<<31):
             return 0
         return multiplier * int(num)

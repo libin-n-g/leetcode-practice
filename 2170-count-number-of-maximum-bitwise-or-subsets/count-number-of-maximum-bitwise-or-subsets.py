df5a853ca872d51f81a08b0d95a44395ede05728
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-07-28 11:29:04 -0700

    Time: 303 ms (36.86%) | Memory: 17.8 MB (73.93%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        max_or = reduce(lambda x, y : x | y, nums)
+        N = len(nums)
+        results = 0
+        def find_subsets(subsets=[], index=0, prev_xor=0):
+            result = 0
+            if prev_xor == max_or:
+                result +=  1
+            for i in range(index, N):
+                subsets.append(nums[i])
+                result += find_subsets(subsets, i+1, prev_xor | nums[i])
+                subsets.pop()
+            return result
+        return find_subsets()

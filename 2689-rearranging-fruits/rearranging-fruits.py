89307a8e87c08bce0649859dea487962c9b7dbef
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-02 14:14:35 -0700

    Time: 114 ms (19.11%) | Memory: 43.4 MB (54.14%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.py
+++ 2689-rearranging-fruits/rearranging-fruits.py
@@ -5,6 +5,7 @@ class Solution:
         for item in basket2:
             counter[item] -= 1
         trasfer_array = []
+        trasfer_count_half = 0
         for item in counter:
             if counter[item] % 2 == 1:
                 return -1
@@ -12,10 +13,10 @@ class Solution:
             while counter[item] != 0:
                 trasfer_array.append(item)
                 counter[item] -= 2
+                trasfer_count_half += 0.5
         trasfer_array.sort()
-        # print(trasfer_array)
         cost = 0
-        for i in trasfer_array[:len(trasfer_array)//2]:
+        for i in trasfer_array[:int(trasfer_count_half)]:
             if i > 2*min_value:
                 cost += 2*min_value
             else:

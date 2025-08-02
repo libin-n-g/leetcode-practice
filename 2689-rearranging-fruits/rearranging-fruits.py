807705ad31f867a60c51af4fbe98dd55ea3ff70d
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-02 14:17:16 -0700

    Time: 75 ms (98.73%) | Memory: 43.3 MB (54.14%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.py
+++ 2689-rearranging-fruits/rearranging-fruits.py
@@ -5,20 +5,12 @@ class Solution:
         for item in basket2:
             counter[item] -= 1
         trasfer_array = []
-        trasfer_count_half = 0
-        for item in counter:
-            if counter[item] % 2 == 1:
+        for item, freq in counter.items():
+            if freq % 2 == 1:
                 return -1
-            counter[item] = abs(counter[item])
-            while counter[item] != 0:
-                trasfer_array.append(item)
-                counter[item] -= 2
-                trasfer_count_half += 0.5
+            trasfer_array.extend([item] * (abs(freq) // 2))
         trasfer_array.sort()
         cost = 0
-        for i in trasfer_array[:int(trasfer_count_half)]:
-            if i > 2*min_value:
-                cost += 2*min_value
-            else:
-                cost += i
+        for i in trasfer_array[:len(trasfer_array)//2]:
+            cost += min(2*min_value, i)
         return cost

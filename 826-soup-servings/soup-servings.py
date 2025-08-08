98959340f3701a09ccd7c30df765f9a16f8c324e
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-07 19:23:48 -0700

    Time: 11 ms (57.43%) | Memory: 20.2 MB (33.33%) - LeetSync

---
+++ 826-soup-servings/soup-servings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n > 5000:
+            return 1
+        @cache
+        def calc(a, b):
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0:
+                return 1
+            if b <= 0:
+                return 0
+            return (calc(a - 100, b) + calc(a - 75, b - 25) + calc(a - 50, b - 50) + calc(a - 25, b - 75))/4
+        return calc(n, n)
+            
+        

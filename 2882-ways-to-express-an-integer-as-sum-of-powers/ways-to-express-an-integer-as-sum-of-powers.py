cb96dc9ac857cef823f64799b2201fb31afdcab5
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-12 14:16:22 -0700

    Time: 285 ms (83.90%) | Memory: 17.9 MB (70.34%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,24 +1,24 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
         MOD = 10**9 + 7
-        # dp = [0] * (n+1)
-        # dp[0] = 1
-        # for i in range(n):
-        #     if i**x > n:
-        #         break
-        #     dp[i] = dp() 
-        @cache
-        def get_ways(n, total, i):
-            new_total = total + i**x
-            if new_total == n:
-                return 1
-            if new_total > n:
-                return 0
-            return (get_ways(n, total, i+1) + get_ways(n, new_total, i+1)) % MOD
-        return get_ways(n, 0, 1)
-        # xss = set(map(lambda i: i**x, list(range(1, x))))
-        # count = 0
-        # for xs in xss:
-        #     if n - xs in xss:
-        #         count += 1
+        dp = [0] * (n+1)
+        dp[0] = 1
+        for i in range(1, n+1):
+            power =  i**x 
+            if power > n:
+                break
+            for j in range(n, power - 1, -1):
+                dp[j] = (dp[j] + dp[j - power]) % MOD
+            # print(dp)
+        return dp[-1]
+        # @cache
+        # def get_ways(n, total, i):
+        #     new_total = total + i**x
+        #     if new_total == n:
+        #         return 1
+        #     if new_total > n:
+        #         return 0
+        #     return (get_ways(n, total, i+1) + get_ways(n, new_total, i+1)) % MOD
+        # return get_ways(n, 0, 1)
+        
 

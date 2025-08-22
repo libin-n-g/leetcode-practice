5f05888d2dfa88f4caa7226d0fce0846ca51ab7a
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-22 15:58:11 -0700

    Time: 31 ms (92.91%) | Memory: 18.7 MB (39.33%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
@@ -10,11 +10,22 @@ class Solution:
                     histogram[j] += 1
             stack = [(-1, -1, 0)] # height, index, previous_count
             for j, val in enumerate(histogram):
+                # find the index where height decreses.
                 while stack[-1][0] >= val:
                     stack.pop()
                 height, index, previous_count = stack[-1]
+                # (j - index) is gap. 
+                # Note that placeholder -1 will helps in getting correct gap when there is no element less that current
+                """
+                aSSUME BELOW STRUCTURE IN MATRIX
+                      |  |  |  |
+                |  |  |  |  |  |
+                |  | a|  |  | b|
+                Gap is 3 here. Total number of submtrix ending at b is (3*b) + a. Let a and b be 1 heights(histogram)
+                """
                 curr_count = val * (j - index) + previous_count
                 count += curr_count
-                stack.append((val, j, curr_count))
+                stack.append((val, j, curr_count)) # Note that the j (index) only increses.
+                # hence stack will contain highest index at left most and decreses as it gets poped.
         return count
                 

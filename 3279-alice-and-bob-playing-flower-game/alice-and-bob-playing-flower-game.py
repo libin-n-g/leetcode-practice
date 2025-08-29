b32e107255a8d2c360a0e21fbd378ad6862d9135
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-28 19:06:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.61%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,9 +1,3 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        count = 0
-        for x in range(1, n+1):
-            if x % 2 == 0:
-                count += (m+1) // 2
-            else:
-                count += m // 2
-        return count
+        return (n*m) // 2

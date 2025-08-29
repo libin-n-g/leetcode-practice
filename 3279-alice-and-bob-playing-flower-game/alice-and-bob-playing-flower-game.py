60c188c53f95cff30f447d8975510b5e05528568
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-28 19:08:35 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.11%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,3 +1,6 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
+        """
+        given an x, we can have m // 2 choices of y. Hence total choice = n* (m//2)
+        """
         return (n*m) // 2

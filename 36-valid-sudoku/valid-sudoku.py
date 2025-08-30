87a1dadc1810fa1e716c78673727305279f0cdba
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-08-29 17:17:58 -0700

    Time: 11 ms (9.87%) | Memory: 17.7 MB (69.74%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rule_checker = defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                box_num = (i // 3) * 3 + j // 3
+                keys = [f"col{j}", f"row{i}", f"box{box_num}"]
+                for k in keys:
+                    if board[i][j] in rule_checker[k]:
+                        print(i, j, board[i][j], keys)
+                        print(rule_checker)
+                        return False
+                    rule_checker[k].add(board[i][j])
+        return True

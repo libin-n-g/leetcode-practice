bc937198e75177b082eb6fbb5095e288ce4a2101
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-03 13:26:51 -0700

    Time: 985 ms (96.54%) | Memory: 18.3 MB (84.85%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
@@ -2,18 +2,20 @@ class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
         # Sort the points by their x-coordinate, and then by their y-coordinate in descending order
         points.sort(key=lambda point: (point[0], -point[1]))
-        ans = 0  # Initialize the number of valid pairs to zero
-      
-        # Iterate over each point
+        count = 0  
+        # Lets select point A where Alice is at. 
+        # Note that it starts from top left corner (because of sorting)
         for i, (_, y1) in enumerate(points):
-            max_y = -inf  # Initialize the maximum y-value for the points considered so far
-
-            # Compare with subsequent points to check for pairs
-            for _, y2 in points[i + 1:]:
-                # If the y-value of the current point is greater than max_y
-                # and not greater than y1, this is a valid pair
-                if max_y < y2 <= y1:
-                    max_y = y2  # Update max_y to the current y-value
-                    ans += 1  # Increment the count of valid pairs
-
-        return ans  # Return the total number of valid pairs found
+            # Now lets select a point which is right and down of point B (x2, y2) where bob is at.
+            # x2 >= x1 and y2 <= y1 (always true because of sorting)
+            y_max = -inf
+            for _, y2 in points[i+1:]:
+                # if x3, y3 is another point other than A and B, 
+                # then we need to make sure the points does not satisfy the following condition
+                # x2 >= x3 >= x1 and y2 <= y3 <= y1.
+                # For any point we have seen before (x3, y3) current B should have x2 >= x3. 
+                # if x2 == x3 then y2 <= y3. First condition is anyway satisfied weneed to count points not satifiing second condition. 
+                if y_max < y2 <= y1:
+                    count += 1
+                    y_max = y2
+        return count  # Return the total number of valid pairs found

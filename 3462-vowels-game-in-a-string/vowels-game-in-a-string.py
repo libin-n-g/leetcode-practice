4bea3a7b4f9435a3c0e522801e31730c8a30e65a
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-12 14:07:27 -0700

    Time: 4 ms (85.80%) | Memory: 18.3 MB (8.16%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -3,6 +3,7 @@ class Solution:
         # c = Counter(s)
         # vowel_count = c["a"] + c["e"] + c["i"] + c["o"] + c["u"]
         vowel_count = s.count('a') + s.count('e') + s.count('i') + s.count('o') + s.count('u')
+        # Count function for string is faster as the function runs in Cpython. 
         if vowel_count == 0:
             return False
         return True

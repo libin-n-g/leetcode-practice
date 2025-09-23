dc173ae2be3612ed522fef1e120ab926fe9d6281
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-23 13:16:41 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.18%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -7,11 +7,8 @@ class Solution:
             v1_split.append(0)
         while len(v2_split) < max_range:
             v2_split.append(0)
-        print(v1_split)
-        print(v2_split)
         for v1, v2 in zip(v1_split, v2_split):
             v1, v2 = int(v1), int(v2)
-            print( v1 == v2,  v1 < v2,  v1> v2)
             if v1 == v2:
                 continue
             elif v1 < v2:

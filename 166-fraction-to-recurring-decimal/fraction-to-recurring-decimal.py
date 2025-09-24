79a8d59bd6242716309c4c519b553d93da8d3619
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-24 11:24:55 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (39.42%) - LeetSync

---
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.py
@@ -0,0 +1,36 @@
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        negative = False
+        if numerator < 0:
+            numerator = - numerator
+            negative = not negative
+        if denominator < 0:
+            negative = not negative
+            denominator = - denominator
+        result = str(numerator//denominator)
+        numerator = numerator % denominator
+        fractions = []
+        numerator_list = [numerator]
+        seen_numerator = set(numerator_list)
+        while numerator:
+            numerator = 10*numerator
+            if numerator in seen_numerator:
+                numerator = numerator // 10
+                break
+            seen_numerator.add(numerator)
+            curr_fraction = (str((numerator)//denominator))
+            numerator = numerator % denominator
+            fractions.append(curr_fraction)
+            numerator_list.append(numerator)
+        print(numerator_list, fractions, numerator)
+        if fractions:
+            result += "."
+        for num, frac in zip(numerator_list, fractions):
+            if num == numerator != 0:
+                result += "("
+            result += frac
+        if "(" in result:
+            result += ")"
+        if negative and result != "0":
+            result = "-" + result
+        return result

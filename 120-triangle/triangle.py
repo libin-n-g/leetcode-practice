c845b9685f23d58a9f7b7b3fcaf03b90aec10398
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-25 10:37:27 -0700

    Time: 11 ms (18.04%) | Memory: 21.5 MB (8.65%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -7,4 +7,5 @@ class Solution:
                 return triangle[level][index] + \
                 min(get_mininum_total(level + 1, index), get_mininum_total(level + 1, index + 1))
             return triangle[level][index]
+        get_mininum_total.cache_clear()
         return get_mininum_total(0, 0)

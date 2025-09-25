3fcd30f9dce8991a53009685a67ce422f0212ee4
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-25 10:36:03 -0700

    Time: 11 ms (18.04%) | Memory: 21.3 MB (8.65%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        @cache
+        def get_mininum_total(level, index):
+            if level + 1 < n:
+                return triangle[level][index] + \
+                min(get_mininum_total(level + 1, index), get_mininum_total(level + 1, index + 1))
+            return triangle[level][index]
+        return get_mininum_total(0, 0)

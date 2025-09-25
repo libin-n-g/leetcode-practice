673329040b13e5898b42092f21442fdf985c301d
LIBIN N GEORGE <libinngeorge@gmail.com>
2025-09-25 10:48:11 -0700

    Time: 3 ms (86.00%) | Memory: 18.7 MB (90.91%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,19 +1,19 @@
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # Get the number of rows in the triangle
-        n = len(triangle)
+    # def minimumTotal(self, triangle: List[List[int]]) -> int:
+    #     # Get the number of rows in the triangle
+    #     n = len(triangle)
         
-        @cache  # Memoize the recursive function to avoid redundant calculations
-        def get_mininum_total(level, index):
-            # Base case: if at the last row, return the value at the current index
-            if level + 1 < n:
-                # Recursive case: add current value to the minimum of the two possible paths below
-                return triangle[level][index] + \
-                       min(get_mininum_total(level + 1, index), get_mininum_total(level + 1, index + 1))
-            return triangle[level][index]
-        ret = get_mininum_total(0, 0)
-        get_mininum_total.cache_clear()
-        return ret
+    #     @cache  # Memoize the recursive function to avoid redundant calculations
+    #     def get_mininum_total(level, index):
+    #         # Base case: if at the last row, return the value at the current index
+    #         if level + 1 < n:
+    #             # Recursive case: add current value to the minimum of the two possible paths below
+    #             return triangle[level][index] + \
+    #                    min(get_mininum_total(level + 1, index), get_mininum_total(level + 1, index + 1))
+    #         return triangle[level][index]
+    #     ret = get_mininum_total(0, 0)
+    #     get_mininum_total.cache_clear()
+    #     return ret
 
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle[-1])
